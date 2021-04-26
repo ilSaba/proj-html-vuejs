0ff19fccfad456c36a4bf2e9e98a39cc47823f32
Andrea Sabatini <sabatini.andrea@outlook.it>
2021-04-26 16:40:55 +0200

    Dettagli migliorati

--- dist/js/main.js
+++ dist/js/main.js
@@ -29,10 +29,17 @@ var app = new Vue({
       name: 'Shop',
       url: '#'
     }],
-    linkFoot: [{
-      name: 'Explore',
+    linkFootAddress: [{
+      name: '382 NE 191st St # 87394 Miami, FL 33179-3899',
       url: '#'
     }, {
+      name: '+1  (305) 547-9909  (9am - 5pm EST, Monday - Friday)',
+      url: '#'
+    }, {
+      name: 'support@maxcoach.com',
+      url: '#'
+    }],
+    linkFootOne: [{
       name: 'Start here',
       url: '#'
     }, {
@@ -41,9 +48,31 @@ var app = new Vue({
     }, {
       name: 'About us',
       url: '#'
+    }],
+    linkFootTwo: [{
+      name: 'Success story',
+      url: '#'
+    }, {
+      name: 'Courses',
+      url: '#'
+    }, {
+      name: 'Contact us',
+      url: '#'
+    }],
+    linkFootThree: [{
+      name: 'Membership',
+      url: '#'
+    }, {
+      name: 'Purchase guide',
+      url: '#'
+    }, {
+      name: 'Privacy policy',
+      url: '#'
+    }, {
+      name: 'Terms of services',
+      url: '#'
     }]
-  },
-  methods: {}
+  }
 });
 
 /***/ }),
--- src/main.js
+++ src/main.js
@@ -29,11 +29,22 @@ var app = new Vue({
             },
         ],
 
-        linkFoot: [
+        linkFootAddress: [
             {
-                name: 'Explore',
+                name: '382 NE 191st St # 87394 Miami, FL 33179-3899',
                 url: '#',
             },
+            {
+                name: '+1  (305) 547-9909  (9am - 5pm EST, Monday - Friday)',
+                url: '#',
+            },
+            {
+                name: 'support@maxcoach.com',
+                url: '#',
+            },
+        ],
+
+        linkFootOne: [
             {
                 name: 'Start here',
                 url: '#',
@@ -46,9 +57,40 @@ var app = new Vue({
                 name: 'About us',
                 url: '#',
             },
+        ],
+
+        linkFootTwo: [
+            {
+                name: 'Success story',
+                url: '#',
+            },
+            {
+                name: 'Courses',
+                url: '#',
+            },
+            {
+                name: 'Contact us',
+                url: '#',
+            },
+        ],
+
+        linkFootThree: [
+            {
+                name: 'Membership',
+                url: '#',
+            },
+            {
+                name: 'Purchase guide',
+                url: '#',
+            },
+            {
+                name: 'Privacy policy',
+                url: '#',
+            },
+            {
+                name: 'Terms of services',
+                url: '#',
+            },
         ]
     },
-
-    methods: {}
-
 })
